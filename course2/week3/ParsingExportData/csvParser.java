6fadf41649ca59ab73df251f7abc176e9097ec3b
Clint Looney <clintlooney@gmail.com>
2019-02-21 18:24:49 -0800

    Initial commit

---
+++ course2/week3/ParsingExportData/csvParser.java
@@ -0,0 +1,12 @@
+
+// By Clint Looney (clooney@)
+// Assignment URL: https://www.coursera.org/learn/java-programming/supplement/Qu17T/programming-exercise-parsing-export-data
+import edu.duke.*;
+import org.apache.commons.csv.*;
+
+public class csvParser {
+    public void readCSV() {
+        FileResource fr = new FileResource();
+        CSVParser parser = new CSVParser();
+    }
+}
